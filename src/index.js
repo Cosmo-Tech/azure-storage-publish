b6c401648245c71acb443a281da8da2299424988
Vincent Carluer <vcarluer@gmail.com>
2021-09-29 23:08:27 +0200

    space

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
+
 const {BlobServiceClient, StorageSharedKeyCredential, BlobSASPermissions, generateBlobSASQueryParameters} = require('@azure/storage-blob');
 const fs = require('fs');
 const path = require('path');
